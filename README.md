a4bf6945569e3e6719d518ad7b77e9d95e233d27
Nagelisious <alexandernagel101@gmail.com>
2022-02-16 10:12:51 -0500

    JUnit testing

---
+++ README.md
@@ -0,0 +1,3 @@
+This is for <h1>JUnit Testing</h2>
+JUNIT 5- Jupiter
+JDK 1.8
---
+++ src/test/java/com/training/pms/CalculatorTest.java
@@ -0,0 +1,51 @@
+package com.training.pms;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	Calculator calc = new Calculator();
+	@Test
+	void testAdd() {
+		int expected = 20;
+		int actual = calc.add(10, 10);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAdd2() {
+		int expected = 20;
+		int actual = calc.add(12, 8);
+		assertEquals(expected, actual);
+	}
+	
+	@Test
+	void testAdd3() {
+		int expected = 20;
+		int actual = calc.add(20, 0);
+		assertEquals(expected, actual);
+	}
+
+//	@Test
+//	void testMul() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testSub() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testDiv() {
+//		fail("Not yet implemented");
+//	}
+//
+//	@Test
+//	void testIsEven() {
+//		fail("Not yet implemented");
+//	}
+
+}
